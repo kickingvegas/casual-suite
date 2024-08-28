cca0fe1c6465081f0f5dcf9a735d9fb081cd7b23
Charles Choi <kickingvegas@gmail.com>
2024-08-28 16:41:10 -0700

    Copy Edits
    - Update requirements.
    - Add note on updating built-in packages.

--- lisp/casual-suite.el
+++ lisp/casual-suite.el
@@ -56,6 +56,12 @@
 ;; (keymap-set reb-lisp-mode-map "C-o" #'casual-re-builder-tmenu)
 ;; (keymap-set bookmark-bmenu-mode-map "C-o" #'casual-bookmarks-tmenu)
 
+;; NOTE: This package requires `casual-lib' which in turn requires an update of
+;; the built-in package `transient' ≥ 0.6.0. Please customize the variable
+;; `package-install-upgrade-built-in' to t to allow for `transient' to be
+;; updated. For further details, consult the INSTALL section of this package's
+;; README.
+
 ;;; Code:
 
 (require 'casual-calc)
